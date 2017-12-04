f7b599bb515ecdadfb698354538e102a1b7dbd24
Chris LaRose <cjlarose@gmail.com>
2017-12-03 21:27:32 -0800

    Convert passphrases to lists of words sooner

--- src/Day4/Main.hs
+++ src/Day4/Main.hs
@@ -2,18 +2,17 @@ module Day4.Main where
 
 import Data.List (nubBy, sort)
 
-isValidPassphrase :: (String -> String -> Bool) -> String -> Bool
-isValidPassphrase wordEquality phrase = xs == nubBy wordEquality xs
-  where xs = words phrase
+isValidPassphrase :: (String -> String -> Bool) -> [String] -> Bool
+isValidPassphrase wordEquality xs = xs == nubBy wordEquality xs
 
-containsNoDuplicates :: String -> Bool
+containsNoDuplicates :: [String] -> Bool
 containsNoDuplicates = isValidPassphrase (==)
 
-containsNoAnagrams :: String -> Bool
+containsNoAnagrams :: [String] -> Bool
 containsNoAnagrams = isValidPassphrase (\a b -> sort a == sort b)
 
 solve :: String -> IO ()
 solve input = do
-  let passphrases = lines input
+  let passphrases = map words . lines $ input
   print . length . filter containsNoDuplicates $ passphrases
   print . length . filter containsNoAnagrams $ passphrases
