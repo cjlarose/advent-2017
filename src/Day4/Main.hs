571cc48b426759876e236165dbb997c5ae604178
Chris LaRose <cjlarose@gmail.com>
2017-12-03 21:18:16 -0800

    Complete Day 4, Part 2

--- src/Day4/Main.hs
+++ src/Day4/Main.hs
@@ -1,12 +1,19 @@
 module Day4.Main where
 
-import Data.List (nub)
+import Data.List (nubBy, sort)
 
-isValidPassphrase :: String -> Bool
-isValidPassphrase phrase = length xs == length (nub xs)
+isValidPassphrase :: (String -> String -> Bool) -> String -> Bool
+isValidPassphrase wordEquality phrase = xs == nubBy wordEquality xs
   where xs = words phrase
+
+containsNoDuplicates :: String -> Bool
+containsNoDuplicates = isValidPassphrase (==)
+
+containsNoAnagrams :: String -> Bool
+containsNoAnagrams = isValidPassphrase (\a b -> sort a == sort b)
 
 solve :: String -> IO ()
 solve input = do
   let passphrases = lines input
-  print . length . filter isValidPassphrase $ passphrases
+  print . length . filter containsNoDuplicates $ passphrases
+  print . length . filter containsNoAnagrams $ passphrases
