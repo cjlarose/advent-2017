f24066fe7a69a4bd25fe616df0573fb99e1c9d87
Chris LaRose <cjlarose@gmail.com>
2017-12-06 08:08:38 -0800

    Revert "Add silly animation"
    This reverts commit 4a1b7a271d457dcb7f53de683ef4d99c2136e928.

--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -1,13 +1,9 @@
 module Day6.Main where
 
 import Data.Set (Set, member, insert, empty)
-import Data.List (maximumBy, intercalate)
+import Data.List (maximumBy)
 import Data.Ord (comparing)
 import Data.Array.IArray (Array, (//), listArray, (!), bounds, assocs)
-import Control.Monad (forM_)
-import Text.Printf (printf)
-import Control.Concurrent (threadDelay)
-import System.IO (stdout, hFlush)
 
 type BankList = Array Int Int
 
@@ -34,28 +30,11 @@ takeWhileDistinct = f empty
 redistributeUntilLoopDetected :: BankList -> [BankList]
 redistributeUntilLoopDetected = takeWhileDistinct . iterate redistribute
 
-showBanks :: BankList -> String
-showBanks banks = unlines rows
- where
-  n = snd (bounds banks)
-  row i =
-    intercalate "  " $ map (\b -> if banks ! b >= i then "x" else " ") [0 .. n]
-  labels    = concatMap (printf "%-3d") ([1 .. (n + 1)] :: [Int])
-  separator = replicate (3 * (n + 1)) '-'
-  rows      = (reverse $ map row [1 .. 15] :: [String]) ++ [separator, labels]
-
 solve :: String -> IO ()
 solve input = do
   let xs       = map read . words $ input
       maxIndex = length xs - 1
       banks    = listArray (0, maxIndex) xs
       states   = redistributeUntilLoopDetected banks
-  forM_
-    (zip states [1 ..])
-    ( \(banks, i) -> do
-      putStrLn . showBanks $ banks
-      print i
-      hFlush stdout
-      threadDelay 5000
-    )
+  print . length $ states
   print . length . redistributeUntilLoopDetected . last $ states
