0fb09259b5d8915ad8a52cd32d2d0a5c4cf2140b
Chris LaRose <cjlarose@gmail.com>
2017-12-08 09:06:43 -0800

    Complete Day 7, Part 2

--- src/Day7/Main.hs
+++ src/Day7/Main.hs
@@ -28,30 +28,30 @@ progWeight :: WeightMap -> AdjacencyList -> String -> Int
 progWeight w adj prog =
   (w Map.! prog) + (sum . map (progWeight w adj) $ childrenPrograms adj prog)
 
+findProgram :: WeightMap -> AdjacencyList -> String -> (String, Int)
+findProgram w adj root = findProgram' w adj root 0
+
+findProgram' :: WeightMap -> AdjacencyList -> String -> Int -> (String, Int)
+findProgram' w adj root siblingWeight = if null bad
+  then (root, siblingWeight - (sum . map getWeight $ children))
+  else findProgram' w adj (head bad) (getWeight (head good))
+ where
+  children    = childrenPrograms adj root
+  getWeight   = progWeight w adj
+  (bad, good) = partition
+    (\p -> notElem (getWeight p) . map getWeight . filter (/=p) $ children)
+    children
+
 solve :: String -> IO ()
 solve input = do
   let parseResult = parseOnly programList (fromString input)
   case parseResult of
     Left  err   -> print err
     Right progs -> do
-      let
-        progNames   = Set.fromList $ map (\(name, _, _) -> name) progs
-        dependants  = Set.fromList $ concatMap (\(_, _, deps) -> deps) progs
-        root        = Set.findMin $ Set.difference progNames dependants
-        adjList     = concatMap (\(p, _, cs) -> [ (p, c) | c <- cs ]) progs
-        weights     = Map.fromList $ map (\(name, w, _) -> (name, w)) progs
-        children    = childrenPrograms adjList root
-        getWeight   = progWeight weights adjList
-        (bad, good) = partition
-          ( \p ->
-            notElem (getWeight p) . map getWeight . filter (/=p) $ children
-          )
-          children
-        badWeight = weights Map.! head bad
-        reqChange = (getWeight . head $ good) - (getWeight . head $ bad)
+      let progNames  = Set.fromList $ map (\(name, _, _) -> name) progs
+          dependants = Set.fromList $ concatMap (\(_, _, deps) -> deps) progs
+          root       = Set.findMin $ Set.difference progNames dependants
+          adjList    = concatMap (\(p, _, cs) -> [ (p, c) | c <- cs ]) progs
+          weights    = Map.fromList $ map (\(name, w, _) -> (name, w)) progs
       print root
-      print children
-      print . map getWeight $ children
-      print bad
-      print good
-      print $ badWeight + reqChange
+      print . findProgram weights adjList $ root
