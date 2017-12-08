e53f3962b6f90db7c2ef9612c7d9fc7fa2ca7cc0
Chris LaRose <cjlarose@gmail.com>
2017-12-08 00:14:58 -0800

    Day 7 WIP
    I realize now I don't understand the problem

--- src/Day7/Main.hs
+++ src/Day7/Main.hs
@@ -1,11 +1,13 @@
 module Day7.Main where
 
 import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+import Data.List (partition)
 import Data.Attoparsec.ByteString (Parser, takeWhile1, inClass, parseOnly, string, option, sepBy1, sepBy, endOfInput)
 import Data.Attoparsec.ByteString.Char8 (char, isDigit_w8)
 import Data.ByteString.UTF8 (fromString, toString)
 
-programName = takeWhile1 $ inClass "a-z"
+programName = toString <$> takeWhile1 (inClass "a-z")
 weight = read . toString <$> takeWhile1 isDigit_w8 :: Parser Int
 arrow = string (fromString " -> ")
 dependantsList = programName `sepBy1` string (fromString ", ")
@@ -16,13 +18,40 @@ programDescription =
     <*> option [] (arrow *> dependantsList)
 programList = programDescription `sepBy` char '\n' <* char '\n' <* endOfInput
 
+type WeightMap = Map.Map String Int
+type AdjacencyList = [(String, String)]
+
+childrenPrograms :: AdjacencyList -> String -> [String]
+childrenPrograms adj prog = map snd . filter (\(p, c) -> p == prog) $ adj
+
+progWeight :: WeightMap -> AdjacencyList -> String -> Int
+progWeight w adj prog =
+  (w Map.! prog) + (sum . map (progWeight w adj) $ childrenPrograms adj prog)
+
 solve :: String -> IO ()
 solve input = do
   let parseResult = parseOnly programList (fromString input)
   case parseResult of
     Left  err   -> print err
     Right progs -> do
-      let progNames  = Set.fromList $ map (\(name, _, _) -> name) progs
-          dependants = Set.fromList $ concatMap (\(_, _, deps) -> deps) progs
-          root       = Set.findMin $ Set.difference progNames dependants
+      let
+        progNames   = Set.fromList $ map (\(name, _, _) -> name) progs
+        dependants  = Set.fromList $ concatMap (\(_, _, deps) -> deps) progs
+        root        = Set.findMin $ Set.difference progNames dependants
+        adjList     = concatMap (\(p, _, cs) -> [ (p, c) | c <- cs ]) progs
+        weights     = Map.fromList $ map (\(name, w, _) -> (name, w)) progs
+        children    = childrenPrograms adjList root
+        getWeight   = progWeight weights adjList
+        (bad, good) = partition
+          ( \p ->
+            notElem (getWeight p) . map getWeight . filter (/=p) $ children
+          )
+          children
+        badWeight = weights Map.! head bad
+        reqChange = (getWeight . head $ good) - (getWeight . head $ bad)
       print root
+      print children
+      print . map getWeight $ children
+      print bad
+      print good
+      print $ badWeight + reqChange
