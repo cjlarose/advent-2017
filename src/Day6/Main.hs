9a0ced73beba9ad5df851a63c3d420db966e1f94
Chris LaRose <cjlarose@gmail.com>
2017-12-08 21:19:52 -0800

    Fix warnings for Day 6

--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -1,17 +1,17 @@
 module Day6.Main where
 
-import Data.Set (Set, member, insert, empty)
+import Data.Set (member, insert, empty)
 import qualified Data.Heap as Heap
 
 type BankList = Heap.Heap (Int, Int)
 
 dropBlocks :: BankList -> Int -> Int -> BankList
 dropBlocks banks i n = Heap.map f banks
-  where
-    sz = Heap.size banks
-    newBlocksAtDistance d = n `div` sz + (if d < n `mod` sz then 1 else 0)
-    distance j = if j >= i then j - i else (j + sz) - i
-    f (m, j) = (- ((- m) + (newBlocksAtDistance . distance $ j)), j)
+ where
+  sz = Heap.size banks
+  newBlocksAtDistance d = n `div` sz + (if d < n `mod` sz then 1 else 0)
+  distance j = if j >= i then j - i else (j + sz) - i
+  f (m, j) = (-((-m) + (newBlocksAtDistance . distance $ j)), j)
 
 redistribute :: BankList -> BankList
 redistribute banks = dropBlocks newHeap (succ i) (-n)
@@ -22,6 +22,7 @@ redistribute banks = dropBlocks newHeap (succ i) (-n)
 takeWhileDistinct :: Ord a => [a] -> [a]
 takeWhileDistinct = f empty
  where
+  f _ [] = []
   f seen (x:xs) | x `member` seen = []
                 | otherwise       = x : f (insert x seen) xs
 
@@ -30,9 +31,8 @@ redistributeUntilLoopDetected = takeWhileDistinct . iterate redistribute
 
 solve :: String -> IO ()
 solve input = do
-  let xs       = map read . words $ input :: [Int]
-      maxIndex = length xs - 1
-      banks    = Heap.fromList $ zipWith (\i c -> (-c, i)) [0 ..] xs
-      states   = redistributeUntilLoopDetected banks
+  let xs     = map read . words $ input :: [Int]
+      banks  = Heap.fromList $ zipWith (\i c -> (-c, i)) [0 ..] xs
+      states = redistributeUntilLoopDetected banks
   print . length $ states
   print . length . redistributeUntilLoopDetected . last $ states
