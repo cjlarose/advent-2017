c35fde1efda1063ae5da339700f566a4c0dc9397
Chris LaRose <cjlarose@gmail.com>
2017-12-08 21:24:21 -0800

    Fix non-exhaustive patterns in Day 5

--- src/Day5/Main.hs
+++ src/Day5/Main.hs
@@ -4,6 +4,7 @@ type Instruction = Int
 type Machine = ([Instruction], [Instruction])
 
 makeMachine :: [Instruction] -> Machine
+makeMachine [] = ([], [])
 makeMachine (x:xs) = ([x], xs)
 
 terminated :: Machine -> Bool
@@ -11,6 +12,7 @@ terminated ([], _) = True
 terminated _       = False
 
 updateCurrent :: (Instruction -> Instruction) -> Machine -> Machine
+updateCurrent _ ([], _) = error "Terminated machine"
 updateCurrent f (x:xs, ys) = (f x : xs, ys)
 
 moveForward :: Machine -> Machine
@@ -18,9 +20,11 @@ moveForward (xs, []  ) = ([], xs)
 moveForward (xs, y:ys) = (y : xs, ys)
 
 moveBackward :: Machine -> Machine
+moveBackward ([], _) = error "Terminated machine"
 moveBackward (x:xs, ys) = (xs, x : ys)
 
 advance :: (Instruction -> Instruction) -> Machine -> Machine
+advance _ ([], _) = error "Terminated machine"
 advance f m@(x:_, _)
   | x >= 0    = iterate moveForward (updateCurrent f m) !! x
   | otherwise = iterate moveBackward (updateCurrent f m) !! (-x)
