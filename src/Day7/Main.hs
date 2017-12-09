a8b98af194329f901d69d3e58fee8106c85ff6ac
Chris LaRose <cjlarose@gmail.com>
2017-12-08 21:15:21 -0800

    Add explicit type signatures for Day 7

--- src/Day7/Main.hs
+++ src/Day7/Main.hs
@@ -3,26 +3,38 @@ module Day7.Main where
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 import Data.List (partition)
+import Data.ByteString (ByteString)
 import Data.Attoparsec.ByteString (Parser, takeWhile1, inClass, parseOnly, string, option, sepBy1, sepBy, endOfInput)
 import Data.Attoparsec.ByteString.Char8 (char, isDigit_w8)
 import Data.ByteString.UTF8 (fromString, toString)
 
+programName :: Parser String
 programName = toString <$> takeWhile1 (inClass "a-z")
-weight = read . toString <$> takeWhile1 isDigit_w8 :: Parser Int
+
+weight :: Parser Int
+weight = read . toString <$> takeWhile1 isDigit_w8
+
+arrow :: Parser ByteString
 arrow = string (fromString " -> ")
+
+dependantsList :: Parser [String]
 dependantsList = programName `sepBy1` string (fromString ", ")
+
+programDescription :: Parser (String, Int, [String])
 programDescription =
   (\a b c -> (a, b, c))
     <$> (programName <* char ' ')
     <*> (char '(' *> weight <* char ')')
     <*> option [] (arrow *> dependantsList)
+
+programList :: Parser [(String, Int, [String])]
 programList = programDescription `sepBy` char '\n' <* char '\n' <* endOfInput
 
 type WeightMap = Map.Map String Int
 type AdjacencyList = [(String, String)]
 
 childrenPrograms :: AdjacencyList -> String -> [String]
-childrenPrograms adj prog = map snd . filter (\(p, c) -> p == prog) $ adj
+childrenPrograms adj prog = map snd . filter ((== prog) . fst) $ adj
 
 progWeight :: WeightMap -> AdjacencyList -> String -> Int
 progWeight w adj prog =
