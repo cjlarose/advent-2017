ff0bd82fd34b2b93d82f5e43d2320e4796728c79
Chris LaRose <cjlarose@gmail.com>
2017-12-09 15:58:57 -0800

    Add monoid instance of RegisterState

--- src/Day8/Interpreter.hs
+++ src/Day8/Interpreter.hs
@@ -1,11 +1,18 @@
 module Day8.Interpreter (interpret, RegisterState(..)) where
 
 import qualified Data.Map.Strict as Map
-import Control.Monad.State (State, execState, get, put)
+import Control.Monad.State (State, execState, get, modify)
 import Control.Monad (when, mapM)
 import Day8.AST (ConditionalStatement(..), RegisterName, BooleanExpression(..), BinOp(..), IncrementStatement(..))
 
 newtype RegisterState = RegisterState (Map.Map RegisterName Int)
+
+instance Monoid RegisterState where
+  mempty = RegisterState Map.empty
+  mappend (RegisterState old) (RegisterState new) = RegisterState $ Map.union new old
+
+binding :: RegisterName -> Int -> RegisterState
+binding reg v = RegisterState $ Map.singleton reg v
 
 lookupRegister :: RegisterName -> State RegisterState Int
 lookupRegister reg = (\(RegisterState m) -> Map.findWithDefault 0 reg m) <$> get
@@ -22,11 +29,8 @@ evaluateBoolExpr :: BooleanExpression -> State RegisterState Bool
 evaluateBoolExpr (BooleanExpression reg op int) = flip (getBinOp op) int <$> lookupRegister reg
 
 evaluateIncrStmt :: IncrementStatement -> State RegisterState ()
-evaluateIncrStmt (IncrementStatement reg int) = do
-  val <- lookupRegister reg
-  let newVal = val + int
-  (RegisterState regs) <- get
-  put $ RegisterState (Map.insert reg newVal regs)
+evaluateIncrStmt (IncrementStatement reg int) =
+  lookupRegister reg >>= (\val -> modify (`mappend`binding reg (val + int)))
 
 evaluate :: ConditionalStatement -> State RegisterState ()
 evaluate (ConditionalStatement b stmt) = do
@@ -34,4 +38,4 @@ evaluate (ConditionalStatement b stmt) = do
   when proceed $ evaluateIncrStmt stmt
 
 interpret :: [ConditionalStatement] -> RegisterState
-interpret xs = execState (mapM evaluate xs) (RegisterState Map.empty)
+interpret xs = execState (mapM evaluate xs) mempty
