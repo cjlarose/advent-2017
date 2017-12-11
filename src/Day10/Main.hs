be32e2fca9277e3d70a20b4d7bd377604da8d7b8
Chris LaRose <cjlarose@gmail.com>
2017-12-10 19:39:01 -0800

    Store Word8 as values instead of Int

--- src/Day10/Main.hs
+++ src/Day10/Main.hs
@@ -2,19 +2,20 @@ module Day10.Main (solve) where
 
 import Data.List (foldl')
 import Data.List.Split (splitOn)
+import Data.Word (Word8)
 import Data.Array.Unboxed (Array, listArray, (!), (//))
 
 data KnotState = KnotState { loopSize :: Int
                            , skipSize :: Int
                            , pos :: Int
-                           , marks :: Array Int Int } deriving Show
+                           , marks :: Array Int Word8 } deriving Show
 
 freshLoop :: Int -> KnotState
 freshLoop n = KnotState
   { loopSize = n
   , skipSize = 0
   , pos      = 0
-  , marks    = listArray (0, n - 1) [0 .. (n - 1)]
+  , marks    = listArray (0, n - 1) . map fromIntegral $ [0 .. (n - 1)]
   }
 
 twist :: KnotState -> Int -> KnotState
@@ -31,7 +32,7 @@ knotHashRound :: KnotState -> [Int] -> KnotState
 knotHashRound = foldl' twist
 
 part1 :: String -> Int
-part1 input = (final ! 0) * (final ! 1)
+part1 input = fromIntegral (final ! 0) * fromIntegral (final ! 1)
  where
   lengths = map read . splitOn "," $ input
   final   = marks $ knotHashRound (freshLoop 256) lengths
