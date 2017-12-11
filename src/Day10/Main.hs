e40134b7b288146c6453e659e6282a59161352c4
Chris LaRose <cjlarose@gmail.com>
2017-12-10 19:02:30 -0800

    Use immutable, unboxed arrays instead of lists in Day 10

--- src/Day10/Main.hs
+++ src/Day10/Main.hs
@@ -2,35 +2,36 @@ module Day10.Main (solve) where
 
 import Data.List (foldl')
 import Data.List.Split (splitOn)
+import Data.Array.Unboxed (Array, listArray, (!), (//))
 
-data KnotState = KnotState { skipSize :: Int
-                           , skipCount :: Int
-                           , marks :: [Int] } deriving Show
+data KnotState = KnotState { loopSize :: Int
+                           , skipSize :: Int
+                           , pos :: Int
+                           , marks :: Array Int Int } deriving Show
 
 freshLoop :: Int -> KnotState
 freshLoop n = KnotState
-  { skipSize  = 0
-  , marks     = [0 .. n]
-  , skipCount = 0
+  { loopSize = n
+  , skipSize = 0
+  , pos      = 0
+  , marks    = listArray (0, n - 1) [0 .. (n - 1)]
   }
-
-advance :: Int -> ([Int] -> [Int]) -> [Int] -> [Int]
-advance k f xs = let (start, rest) = splitAt k xs in rest ++ f start
 
 twist :: KnotState -> Int -> KnotState
-twist s n = KnotState
-  { skipSize  = 1 + skipSize s
-  , marks     = advance (skipSize s) id . advance n reverse $ marks s
-  , skipCount = skipCount s + skipSize s + n
-  }
+twist s k = s { skipSize = 1 + skipSize s
+              , pos      = (pos s + skipSize s + k) `mod` loopSize s
+              , marks    = marks s // changes
+              }
+ where
+  indices   = take k $ iterate (\x -> (x + 1) `mod` loopSize s) (pos s)
+  newValues = reverse $ map (\i -> marks s ! i) indices
+  changes   = zip indices newValues
 
 knotHash :: Int -> [Int] -> Int
-knotHash n xs = x * y
- where
-  final   = foldl' twist (freshLoop n) xs
-  (x:y:_) = drop (n + 1 - (skipCount final `mod` (n + 1))) (marks final)
+knotHash n xs = (final ! 0) * (final ! 1)
+  where final = marks $ foldl' twist (freshLoop n) xs
 
 solve :: String -> IO ()
 solve input = do
   let lengths = map read $ splitOn "," input
-  print . knotHash 255 $ lengths
+  print . knotHash 256 $ lengths
