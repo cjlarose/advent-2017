949cea4e2670079bad2feae494e401dd7dcf55c8
Chris LaRose <cjlarose@gmail.com>
2017-12-12 22:06:52 -0800

    Complete Day 13, Part 2

--- src/Day13/Main.hs
+++ src/Day13/Main.hs
@@ -23,9 +23,23 @@ tripSeverity :: Firewall -> Int
 tripSeverity =
   sum . map (\(d, r) -> if position r d == 0 then d * r else 0) . Map.assocs
 
+smallestPossibleSafeDelay :: Firewall -> PicoTime
+smallestPossibleSafeDelay fw =
+  fst . head . filter ((==0) . snd) . map (\t -> (t, sev t)) $ [0 ..]
+ where
+  sev :: PicoTime -> Int
+  sev t0 =
+    sum
+      . map (\(d, r) -> if position r (d + t0) == 0 then 1 else 0)
+      . Map.assocs
+      $ fw
+
 solve :: String -> IO ()
 solve input = do
   let parsed = parseOnly firewall . pack $ input
   case parsed of
     Left  err -> print err
-    Right ast -> print . tripSeverity . Map.fromList $ ast
+    Right ast -> do
+      let fw = Map.fromList ast
+      print . tripSeverity $ fw
+      print . smallestPossibleSafeDelay $ fw
