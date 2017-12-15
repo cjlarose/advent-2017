f2de6347b39621e257d36adad211b3c130e71d28
Chris LaRose <cjlarose@gmail.com>
2017-12-15 01:18:17 -0800

    Extract some functions

--- src/Day15/Main.hs
+++ src/Day15/Main.hs
@@ -5,22 +5,19 @@ import Data.Bits ((.&.))
 generator :: Int -> Int -> [Int]
 generator factor = drop 1 . iterate (\x -> (x * factor) `mod` 0x7FFFFFFF)
 
+judge :: Int -> Int -> Bool
+judge x y = x .&. 0xFFFF == y .&. 0xFFFF
+
+countMatches :: Int -> [Int] -> [Int] -> Int
+countMatches n genA genB =
+  length . filter (uncurry judge) . take n $ zip genA genB
+
 part1 :: Int -> Int -> Int
 part1 seedA seedB =
-  length
-    . filter (\(x, y) -> x .&. 0xFFFF == y .&. 0xFFFF)
-    . take 40000000
-    $ zip genA genB
- where
-  genA = generator 16807 seedA
-  genB = generator 48271 seedB
+  countMatches 40000000 (generator 16807 seedA) (generator 48271 seedB)
 
 part2 :: Int -> Int -> Int
-part2 seedA seedB =
-  length
-    . filter (\(x, y) -> x .&. 0xFFFF == y .&. 0xFFFF)
-    . take 5000000
-    $ zip genA genB
+part2 seedA seedB = countMatches 5000000 genA genB
  where
   genA = filter (\x -> x `mod` 4 == 0) . generator 16807 $ seedA
   genB = filter (\x -> x `mod` 8 == 0) . generator 48271 $ seedB
