64c1fff65f9d5ee220fd6e414a899c0b76b9dfa5
Chris LaRose <cjlarose@gmail.com>
2017-12-15 01:06:25 -0800

    Complete Day 15, Part 2

--- src/Day15/Main.hs
+++ src/Day15/Main.hs
@@ -15,7 +15,18 @@ part1 seedA seedB =
   genA = generator 16807 seedA
   genB = generator 48271 seedB
 
+part2 :: Int -> Int -> Int
+part2 seedA seedB =
+  length
+    . filter (\(x, y) -> x .&. 0xFFFF == y .&. 0xFFFF)
+    . take 5000000
+    $ zip genA genB
+ where
+  genA = filter (\x -> x `mod` 4 == 0) . generator 16807 $ seedA
+  genB = filter (\x -> x `mod` 8 == 0) . generator 48271 $ seedB
+
 solve :: String -> IO ()
 solve input = do
   let (seedA:seedB:_) = map (read . last . words) . lines $ input
   print $ part1 seedA seedB
+  print $ part2 seedA seedB
