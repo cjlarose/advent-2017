4670867d2a221e75743649572e9dbb179f1b5ab8
Chris LaRose <cjlarose@gmail.com>
2017-12-17 20:35:06 -0800

    Complete Day 17, Part 2

--- src/Day17/Main.hs
+++ src/Day17/Main.hs
@@ -11,12 +11,18 @@ buffers k = map (\(_, _, s) -> s) $ iterate buffers' (1, 0, Seq.singleton 0)
     let newPos = (((pos + k) `mod` n) + 1)
     in  (n + 1, newPos, Seq.insertAt newPos n xs)
 
+valueAfter :: Int -> Seq.Seq Int -> Int
+valueAfter marker s = fromJust
+  $ Seq.lookup (fromJust (Seq.elemIndexL marker s) + 1 `mod` Seq.length s) s
+
 part1 :: Int -> Int
-part1 k = fromJust
-  $ Seq.lookup (fromJust (Seq.elemIndexL 2017 s) + 1 `mod` Seq.length s) s
-  where s = buffers k !! 2017
+part1 k = valueAfter 2017 (buffers k !! 2017)
+
+part2 :: Int -> Int
+part2 k = valueAfter 0 (buffers k !! 50000000)
 
 main :: IO ()
 main = do
   n <- read <$> getFirstLineOfInput "17"
   print . part1 $ n
+  print . part2 $ n
