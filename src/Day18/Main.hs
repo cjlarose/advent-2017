cc53d042669e5edeab49a6dab5eaf97aaf3c5643
Chris LaRose <cjlarose@gmail.com>
2017-12-18 03:52:17 -0800

    Technically complete Day 18, Part 2
    This program only works because Haskell is able to detect that program 0
    and program 1 are in a deadlock. The program prints out whenever a
    program sends or receives data so if you collect the output and pipe to
    `grep '1:sending' | wc -l`, you'll get the answer. It's a huge hack.

--- src/Day18/Main.hs
+++ src/Day18/Main.hs
@@ -5,6 +5,7 @@ import Data.Attoparsec.Text (parseOnly)
 import Day18.AST (Instruction(..))
 import Day18.Parser (instructions)
 import Day18.Machine (Machine, newMachine, readVal, setVal, deref, advancePc, jumpRelative, readPc)
+import Debug.Trace (traceShow)
 
 listen :: Machine -> [Instruction] -> [Int]
 listen m xs = go (xs !! readPc m)
@@ -21,8 +22,34 @@ listen m xs = go (xs !! readPc m)
   go (JGZ a b) =
     next $ (if deref a m > 0 then jumpRelative (deref b m) else advancePc) m
 
+sentValues :: Int -> Machine -> [Instruction] -> [Int] -> [Int]
+sentValues progId m xs inbox = go (xs !! readPc m)
+ where
+  next m' = sentValues progId m' xs inbox
+
+  go (SND ref) =
+    traceShow (show progId ++ ": sending " ++ show (deref ref m)) deref ref m
+      : next (advancePc m)
+  go (SET k ref) = next . advancePc . setVal k (deref ref m) $ m
+  go (ADD k ref) = next . advancePc . setVal k (readVal k m + deref ref m) $ m
+  go (MUL k ref) = next . advancePc . setVal k (readVal k m * deref ref m) $ m
+  go (MOD k ref) =
+    next . advancePc . setVal k (readVal k m `mod` deref ref m) $ m
+  go (RCV k) =
+    let (received:rest) = inbox
+    in  traceShow (show progId ++ ": received " ++ show received)
+                  (sentValues progId (advancePc . setVal k received $ m) xs rest)
+  go (JGZ a b) =
+    next $ (if deref a m > 0 then jumpRelative (deref b m) else advancePc) m
+
 part1 :: [Instruction] -> Int
 part1 xs = last $ listen (newMachine 0) xs
+
+part2 :: [Instruction] -> [Int]
+part2 xs =
+  let m0 = sentValues 0 (newMachine 0) xs m1
+      m1 = sentValues 1 (newMachine 1) xs m0
+  in  m0
 
 main :: IO ()
 main = do
@@ -30,4 +57,6 @@ main = do
   let parsed = parseOnly instructions input
   case parsed of
     Left  err -> print err
-    Right ast -> print . part1 $ ast
+    Right ast -> do
+      print . part1 $ ast
+      print . length . part2 $ ast
