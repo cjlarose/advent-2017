e5d0dc58922e4d3a9fc4d802a28d9e0462275cbe
Chris LaRose <cjlarose@gmail.com>
2017-12-17 22:08:03 -0800

    Complete Day 18, Part 1

--- src/Day18/Main.hs
+++ src/Day18/Main.hs
@@ -47,11 +47,37 @@ instruction = choice
 instructions :: Parser [Instruction]
 instructions = (instruction `sepBy` endOfLine) <* endOfLine <* endOfInput
 
-getRegister :: Char -> Machine -> Int
-getRegister k (_, _, regs) = Map.findWithDefault 0 k regs
+readVal :: Char -> Machine -> Int
+readVal k (_, _, regs) = Map.findWithDefault 0 k regs
 
-runMachineUntilRecover :: Machine -> [Instruction] -> Machine
-runMachineUntilRecover (pc, lastSound, xs) _ = (pc, lastSound, xs)
+deref :: ReferenceOrImmediate -> Machine -> Int
+deref (Reference k) m = readVal k m
+deref (Immediate x) _ = x
+
+advance :: Machine -> [Instruction] -> Either Int Machine
+advance m@(pc, lastSound, regs) xs = go (xs !! pc)
+ where
+  go (Sound k  ) = Right (pc + 1, readVal k m, regs)
+  go (Set k ref) = Right (pc + 1, lastSound, Map.insert k (deref ref m) regs)
+  go (Add k ref) =
+    Right (pc + 1, lastSound, Map.insert k (readVal k m + deref ref m) regs)
+  go (Multiply k ref) =
+    Right (pc + 1, lastSound, Map.insert k (readVal k m * deref ref m) regs)
+  go (Mod k ref) =
+    Right (pc + 1, lastSound, Map.insert k (readVal k m `mod` deref ref m) regs)
+  go (RecoverNZ k) =
+    if readVal k m == 0 then Right (pc + 1, lastSound, regs) else Left lastSound
+  go (JumpGZ k ref) =
+    let newPc = (if readVal k m > 0 then pc + deref ref m else pc + 1)
+    in  Right (newPc, lastSound, regs)
+
+runMachineUntilRecovery :: Machine -> [Instruction] -> Int
+runMachineUntilRecovery m xs = case advance m xs of
+  Left  lastSound  -> lastSound
+  Right newMachine -> runMachineUntilRecovery newMachine xs
+
+part1 :: [Instruction] -> Int
+part1 = runMachineUntilRecovery (0, -1, Map.empty)
 
 main :: IO ()
 main = do
@@ -59,4 +85,4 @@ main = do
   let parsed = parseOnly instructions input
   case parsed of
     Left  err -> print err
-    Right ast -> print ast
+    Right ast -> print . part1 $ ast
