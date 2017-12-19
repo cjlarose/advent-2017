d4f3e76a21c42d3c9000f764c297ff459b03c818
Chris LaRose <cjlarose@gmail.com>
2017-12-18 22:40:30 -0800

    Remove precalculation of letter and plus locations

--- src/Day19/Main.hs
+++ src/Day19/Main.hs
@@ -2,8 +2,6 @@ module Day19.Main (main) where
 
 import Advent2017.Input (getInputAsString)
 import Data.Char (isAlpha, isSpace)
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
 import qualified Data.Array.IArray as Array
 import Data.Array.IArray ((!))
 
@@ -17,12 +15,6 @@ toGrid str = Array.array ((0, 0), (length rows - 1, length (head rows) - 1))
   rows    = lines str
   entries = concat
     $ zipWith (\row i -> zipWith (\c j -> ((i, j), c)) row [0 ..]) rows [0 ..]
-
-findLetters :: Grid -> Map.Map (Int, Int) Char
-findLetters = Map.fromList . filter (isAlpha . snd) . Array.assocs
-
-findPluses :: Grid -> Set.Set (Int, Int)
-findPluses = Set.fromList . map fst . filter ((=='+') . snd) . Array.assocs
 
 move :: (Int, Int) -> (Int, Int) -> (Int, Int)
 move (a, b) (c, d) = (a + c, b + d)
@@ -40,26 +32,13 @@ options g pos going =
 nextDir :: Grid -> (Int, Int) -> (Int, Int) -> (Int, Int)
 nextDir g pos going = head $ options g pos going
 
-routePacket
-  :: (Int, Int)
-  -> Grid
-  -> Map.Map (Int, Int) Char
-  -> Set.Set (Int, Int)
-  -> Direction
-  -> String
-  -> Int
-  -> (String, Int)
-routePacket pos g letters pluses dir ls steps = go
+routePacket :: (Int, Int) -> Grid -> Direction -> String -> Int -> (String, Int)
+routePacket pos g dir ls steps = go
  where
-  continue newDir newLs = routePacket (move pos newDir)
-                                      g
-                                      letters
-                                      pluses
-                                      newDir
-                                      (ls ++ newLs)
-                                      (steps + 1)
-  go | Map.member pos letters   = continue dir [letters Map.! pos]
-     | Set.member pos pluses    = continue (nextDir g pos dir) []
+  continue newDir newLs =
+    routePacket (move pos newDir) g newDir (ls ++ newLs) (steps + 1)
+  go | isAlpha (g ! pos)        = continue dir [g ! pos]
+     | g ! pos == '+'           = continue (nextDir g pos dir) []
      | null (options g pos dir) = (ls, steps)
      | otherwise                = continue dir []
 
@@ -67,8 +46,7 @@ getStartPos :: Grid -> (Int, Int)
 getStartPos = fst . head . filter (\(_, c) -> not . isSpace $ c) . Array.assocs
 
 getRoute :: Grid -> (String, Int)
-getRoute g =
-  routePacket (getStartPos g) g (findLetters g) (findPluses g) (1, 0) "" 0
+getRoute g = routePacket (getStartPos g) g (1, 0) "" 0
 
 main :: IO ()
 main = do
