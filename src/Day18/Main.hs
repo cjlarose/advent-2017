04713ef2815c67c4d1a20afb04b69ca9add33eb9
Chris LaRose <cjlarose@gmail.com>
2017-12-18 21:08:51 -0800

    Complete a proper solution to Day 18, Part 2

--- src/Day18/Main.hs
+++ src/Day18/Main.hs
@@ -5,7 +5,10 @@ import Data.Attoparsec.Text (parseOnly)
 import Day18.AST (Instruction(..))
 import Day18.Parser (instructions)
 import Day18.Machine (Machine, newMachine, readVal, setVal, deref, advancePc, jumpRelative, readPc)
-import Debug.Trace (traceShow)
+import qualified Data.Sequence as Seq
+import Data.Sequence ((|>), ViewL((:<)))
+
+data PausedMachine = Receiving (Int -> PausedMachine) | Sending (Int, Machine)
 
 listen :: Machine -> [Instruction] -> [Int]
 listen m xs = go (xs !! readPc m)
@@ -22,34 +25,49 @@ listen m xs = go (xs !! readPc m)
   go (JGZ a b) =
     next $ (if deref a m > 0 then jumpRelative (deref b m) else advancePc) m
 
-sentValues :: Int -> Machine -> [Instruction] -> [Int] -> [Int]
-sentValues progId m xs inbox = go (xs !! readPc m)
+runUntilCommunicating :: Machine -> [Instruction] -> PausedMachine
+runUntilCommunicating m xs = go (xs !! readPc m)
  where
-  next m' = sentValues progId m' xs inbox
+  next :: Machine -> PausedMachine
+  next m' = runUntilCommunicating m' xs
 
-  go (SND ref) =
-    traceShow (show progId ++ ": sending " ++ show (deref ref m)) deref ref m
-      : next (advancePc m)
+  go :: Instruction -> PausedMachine
+  go (SND ref  ) = Sending (deref ref m, advancePc m)
   go (SET k ref) = next . advancePc . setVal k (deref ref m) $ m
   go (ADD k ref) = next . advancePc . setVal k (readVal k m + deref ref m) $ m
   go (MUL k ref) = next . advancePc . setVal k (readVal k m * deref ref m) $ m
   go (MOD k ref) =
     next . advancePc . setVal k (readVal k m `mod` deref ref m) $ m
   go (RCV k) =
-    let (received:rest) = inbox
-    in  traceShow (show progId ++ ": received " ++ show received)
-                  (sentValues progId (advancePc . setVal k received $ m) xs rest)
+    Receiving (\received -> next . advancePc . setVal k received $ m)
   go (JGZ a b) =
     next $ (if deref a m > 0 then jumpRelative (deref b m) else advancePc) m
 
 part1 :: [Instruction] -> Int
 part1 xs = last $ listen (newMachine 0) xs
 
-part2 :: [Instruction] -> [Int]
-part2 xs =
-  let m0 = sentValues 0 (newMachine 0) xs m1
-      m1 = sentValues 1 (newMachine 1) xs m0
-  in  m0
+part2 :: [Instruction] -> Int
+part2 xs = go (runUntilCommunicating (newMachine 0) xs, Seq.empty)
+              (runUntilCommunicating (newMachine 1) xs, Seq.empty)
+              0
+ where
+  run :: Machine -> PausedMachine
+  run m = runUntilCommunicating m xs
+
+  go
+    :: (PausedMachine, Seq.Seq Int)
+    -> (PausedMachine, Seq.Seq Int)
+    -> Int
+    -> Int
+  go (Sending (v0, m0), inbox0) (m1, inbox1) acc =
+    go (run m0, inbox0) (m1, inbox1 |> v0) acc
+  go (m0, inbox0) (Sending (v1, m1), inbox1) acc =
+    go (m0, inbox0 |> v1) (run m1, inbox1) (acc + 1)
+  go (Receiving f, inbox0) (Receiving g, inbox1) acc
+    | Seq.EmptyL <- Seq.viewl inbox0, Seq.EmptyL <- Seq.viewl inbox1 = acc
+    | v0:<rest0 <- Seq.viewl inbox0 = go (f v0, rest0) (Receiving g, inbox1) acc
+    | v1:<rest1 <- Seq.viewl inbox1 = go (Receiving f, inbox0) (g v1, rest1) acc
+    | otherwise                     = error "unreachable"
 
 main :: IO ()
 main = do
@@ -59,4 +77,4 @@ main = do
     Left  err -> print err
     Right ast -> do
       print . part1 $ ast
-      print . length . part2 $ ast
+      print . part2 $ ast
