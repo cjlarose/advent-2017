4bb3ec0619fb75fcd372cc88dd016d6185a8cfea
Chris LaRose <cjlarose@gmail.com>
2017-12-25 01:47:14 -0700

    Optimize C program. Complete Day 23, Part 2

--- src/Day23/23.c
+++ src/Day23/23.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+int is_composite(int n) {
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 int main() {
   int a = 1;
   int b = 0;
   int c = 0;
-  int d = 0;
-  int e = 0;
   int f = 0;
   int g = 0;
   int h = 0;
@@ -21,24 +28,8 @@ int main() {
   }
 
   while (1) {
-    f = 1;
-    d = 2;
+    f = !is_composite(b);
 
-    do {
-      e = 2;
-
-      do {
-        g = d * e - b;
-        if (g == 0) {
-          f = 0;
-        }
-        e -= -1;
-        g = e - b;
-      } while (g != 0);
-
-      d -= -1;
-      g = d - b;
-    } while (g != 0);
     if (f == 0) {
       h -= -1;
     }
